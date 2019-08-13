b7c40ae76b0d038672441da36fda5908fb76db2c
Jackmc <jackmc1122@gmail.com>
2019-08-13 10:34:27 -0700

    Add Dog component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import Dog from './Dog';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Dog />
+    </>
+  );
 }
-  
---
+++ src/components/Dog.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Dog() {
+  return (
+    <dl>
+      <dt>Name</dt>
+      <dd>Spot</dd>
+
+      <dt>Age</dt>
+      <dd>10</dd>
+
+      <dt>Weight</dt>
+      <dd>50 lbs</dd>
+    </dl>
+  );
+}
