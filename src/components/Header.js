9003df1b75050d2652bcd00beac700e60c6c43d2
Jackmc <jackmc1122@gmail.com>
2019-08-13 10:39:28 -0700

    Add style to the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export default function Header() {
-  return <h1>My Header</h1>;
+  const styles = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '1em',
+    height: '2em',
+  };
+
+  return (
+    <header style={styles}>
+      <h1 style={{ margin: 0 }}>My Dogs</h1>
+    </header>
+  );
 }
