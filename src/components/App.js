8eb26a6eda74977cd4aca2adbc4ded658847aca7
Jackmc <jackmc1122@gmail.com>
2019-08-13 10:35:53 -0700

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './Header';
 import Dog from './Dog';
 
 export default function App() {
   return (
     <>
+      <Header />
       <Dog />
     </>
   );
